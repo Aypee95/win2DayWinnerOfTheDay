a2410a2c42d9c2f0686a8608eb754879e6440b31
Andreas P. <25672097+Aypee95@users.noreply.github.com>
2024-01-16 17:44:23 +0100

    change logon type of scheduled task

--- Install-ScheduledTask.ps1
+++ Install-ScheduledTask.ps1
@@ -3,7 +3,8 @@ $action = New-ScheduledTaskAction -Execute "node" -Argument "index.js" -WorkingD
 $dailyTrigger = New-ScheduledTaskTrigger -Daily -At 4pm
 $startupTrigger = New-ScheduledTaskTrigger -AtStartup
 $taskName = $pwd.path.split('\')[-1]
+$principal = New-ScheduledTaskPrincipal -UserID $env:UserName -LogonType S4U
 $description = "Simple web scraping app with pupeteer to check if you won the checkpot on win2day.at "
 
-$task = New-ScheduledTask -Action $action -Trigger $dailyTrigger, $startupTrigger -Description $description
+$task = New-ScheduledTask -Action $action -Trigger $dailyTrigger, $startupTrigger -Principal $principal -Description $description
 Register-ScheduledTask -TaskName $taskName -InputObject $task -TaskPath "nodejs"
